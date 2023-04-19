c8e988d0d7e8140404eb558bc88b3dea5d0dfa72
“clar9309” <clar9309@stud.kea.dk>
2023-04-19 14:54:45 +0200

    hero component

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,15 +1,17 @@
 <script>
   import Header from "./Header.svelte";
   import Hero from "./Hero.svelte";
-  // import CompBullets from "./CompBullets.svelte";
+  import CompBullets from "./CompBullets.svelte";
   // import OurService from "./OurService.svelte";
   // import OrgChart from "./OrgChart.svelte";
   // import Contact from "./Contact.svelte";
+  import Footer from "./Footer.svelte";
 </script>
 
 <Header />
 <Hero />
-<!-- <CompBullets />
-<OurService />
+<CompBullets />
+<!-- <OurService />
 <OrgChart />
 <Contact /> -->
+<Footer />
--- src/routes/Hero.svelte
+++ src/routes/Hero.svelte
@@ -1,1 +1,64 @@
-<p>This is the hero</p>
+<script>
+  import "../app.css";
+</script>
+
+<section class="bg-primary-purple-300">
+  <div
+    class="grid max-w-screen-xl px-4 py-8 mx-auto lg:gap-8 xl:gap-0 lg:py-16 lg:grid-cols-12"
+  >
+    <div class="mr-auto place-self-center lg:col-span-7">
+      <h1
+        class="max-w-2xl mb-4 text-3xl tracking-tight leading-none md:text-5xl xl:text-6xl text-purple-100 font-ivypresto font-normal"
+      >
+        We digitalize and optimize your hotels systems
+      </h1>
+      <p
+        class="max-w-2xl mb-6 font-light text-purple-50 text-opacity-90 lg:mb-8 md:text-lg lg:text-xl font-montserrat"
+      >
+        Enhance guest experience and security with omnihost (working progress,
+        kommer på noget bedre senere til en brainstorm af en art)
+      </p>
+      <a
+        href="#"
+        class="inline-flex items-center justify-center px-6 py-2 mr-6 text-base font-medium text-center bg-purple-50 text-primary-purple-300 rounded-lg bg-primary-700 hover:bg-primary-800"
+      >
+        Watch Demo
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          width="18"
+          height="18"
+          viewBox="0 0 24 24"
+          class="ml-2"
+          ><path
+            fill="currentColor"
+            d="M12 2C6.48 2 2 6.48 2 12s4.48 10 10 10s10-4.48 10-10S17.52 2 12 2zm-2 13.5v-7a.5.5 0 0 1 .8-.4l4.67 3.5c.27.2.27.6 0 .8l-4.67 3.5a.5.5 0 0 1-.8-.4z"
+          /></svg
+        >
+      </a>
+      <a
+        href="#"
+        class="inline-flex items-center justify-center px-10 py-2 text-base font-medium text-purple-50 border border-gray-300 rounded-lg"
+      >
+        Contact Us
+      </a>
+    </div>
+    <div class="hidden lg:mt-0 lg:col-span-5 lg:flex">
+      <img
+        src="https://flowbite.s3.amazonaws.com/blocks/marketing-ui/hero/phone-mockup.png"
+        alt="mockup"
+      />
+    </div>
+  </div>
+  <div class="items-center justify-between">
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width="42"
+      height="42"
+      viewBox="0 0 256 256"
+      class="fill-purple-50"
+      ><path
+        d="M128 28a100 100 0 1 0 100 100A100.11 100.11 0 0 0 128 28Zm0 192a92 92 0 1 1 92-92a92.1 92.1 0 0 1-92 92Zm34.83-86.83a4 4 0 0 1 0 5.66l-32 32a4 4 0 0 1-5.66 0l-32-32a4 4 0 0 1 5.66-5.66L124 158.34V88a4 4 0 0 1 8 0v70.34l25.17-25.17a4 4 0 0 1 5.66 0Z"
+      /></svg
+    >
+  </div>
+</section>
