7f7d6b34e9caa0383a1055111429aff4699a6799
“clar9309” <clar9309@stud.kea.dk>
2023-04-19 14:07:04 +0200

    header desktop

--- src/routes/Header.svelte
+++ src/routes/Header.svelte
@@ -2,25 +2,26 @@
   import "../app.css";
 </script>
 
-<header class="bg-black">
-  <nav>
-    <div class="logo">Omnihost</div>
-    <ul>
+<header class="bg-primary-purple-300">
+  <nav
+    class="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4"
+  >
+    <div class="text-purple-50 font-medium">OmniHost</div>
+    <ul
+      class="font-medium flex flex-col p-4 md:p-0 mt-4 md:flex-row md:space-x-8 md:mt-0 text-purple-50"
+    >
       <li>
-        <a href="/">Hotels</a>
+        <a href="#">Hotels</a>
       </li>
       <li>
-        <a href="/">Our Service</a>
+        <a href="#">Our Service</a>
       </li>
       <li>
-        <a href="/">Team Omnihost</a>
+        <a href="#">Team Omnichart</a>
       </li>
       <li>
-        <a href="/">Contact</a>
+        <a href="#">Contact</a>
       </li>
     </ul>
   </nav>
 </header>
-
-<style>
-</style>
