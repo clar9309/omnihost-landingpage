54089dcefe79a71472bc1109ffef1424ab8df724
“clar9309” <clar9309@stud.kea.dk>
2023-04-23 11:28:11 +0200

    Comp bullets html/css setup

--- src/routes/CompBullets.svelte
+++ src/routes/CompBullets.svelte
@@ -1,1 +1,20 @@
-<p>This is our bullet points</p>
+<script>
+  import "../app.css";
+</script>
+
+<section class=" text-marine-blue-800 font-sans mt-20">
+  <div class="flex justify-center gap-44">
+    <div class="block text-center">
+      <h5 class="text-6xl mb-2 font-semibold">50%</h5>
+      <p class="opacity-70 text-lg">Faster than documentation</p>
+    </div>
+    <div class="block text-center">
+      <h5 class="text-6xl mb-2 font-semibold">Unlimted</h5>
+      <p class="opacity-70 text-lg">Unlimited integrations</p>
+    </div>
+    <div class="block text-center">
+      <h5 class="text-6xl mb-2 font-semibold">66%</h5>
+      <p class="opacity-70 text-lg">Less minor types</p>
+    </div>
+  </div>
+</section>
