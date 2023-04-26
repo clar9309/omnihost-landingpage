4c497f3aa2031f8d65bef4e234da28626e0d98fa
“clar9309” <clar9309@stud.kea.dk>
2023-04-26 10:59:45 +0200

    Responsive header and hero

--- src/routes/Header.svelte
+++ src/routes/Header.svelte
@@ -52,6 +52,32 @@
           <a class="text-gray-800 hover:text-blue-400" href="/">Team OmniHost</a
           >
           <a class="text-gray-800 hover:text-blue-400" href="/">Contact</a>
+          <div class="flex my-4 sm:flex md:hidden lg:hidden">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width="22"
+              height="22"
+              viewBox="0 0 24 24"
+              class="mr-4 fill-purple-50"
+              ><path
+                d="M19.95 21q-3.225 0-6.287-1.438t-5.425-3.8q-2.363-2.362-3.8-5.425T3 4.05q0-.45.3-.75t.75-.3H8.1q.35 0 .625.225t.325.575l.65 3.5q.05.35-.013.638T9.4 8.45L7 10.9q1.05 1.8 2.625 3.375T13.1 17l2.35-2.35q.225-.225.588-.337t.712-.063l3.45.7q.35.075.575.338T21 15.9v4.05q0 .45-.3.75t-.75.3Z"
+              /></svg
+            >
+            <a href="tel:+4526192327"> +45 26 19 23 27</a>
+          </div>
+          <div class="flex sm:flex md:hidden lg:hidden">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width="22"
+              height="22"
+              viewBox="0 0 24 24"
+              class="mr-4 fill-purple-50"
+              ><path
+                d="M4 20q-.825 0-1.413-.588T2 18V6q0-.825.588-1.413T4 4h16q.825 0 1.413.588T22 6v12q0 .825-.588 1.413T20 20H4Zm8-7l8-5V6l-8 5l-8-5v2l8 5Z"
+              /></svg
+            >
+            <a href="mailto: info@omnihost.app"> info@omnihost.app</a>
+          </div>
         </div>
       </nav>
     </div>
